abbcac870c1d51949ff3dbda37eb0cdbe55e1151
Gwen Bell <gwen@gwenbell.com>
2013-04-13 13:42:23 -0400

    add route in index

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Gwen Bell Dot Com' });
 };
+
+exports.about = function(req, res){
+	res.render('index', { title: 'About Gwen Bell'});
+};
