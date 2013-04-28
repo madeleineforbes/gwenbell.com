8e7cc0c6395d67f2c75611c77710415f0a60002e
Gwen Bell <gwen@gwenbell.com>
2013-04-28 14:42:03 -0400

    update app

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
+app.get('/responses' routes.responses);
 app.get('*', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
