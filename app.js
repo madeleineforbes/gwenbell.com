b0173b685d6114eb1870f06c767ed692d59d8ded
Gwen Bell <gwen@gwenbell.com>
2013-04-29 16:46:34 -0400

    add md route

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/responses', routes.responses);
+app.get('/motherlessdaughters', routes.motherlessdaughters);
 app.get('*', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
