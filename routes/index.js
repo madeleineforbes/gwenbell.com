400900d63d5a230a207e8a43f8727881dcdf16b9
Gwen Bell <gwen@gwenbell.com>
2013-04-29 22:32:35 -0400

    ind*

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,10 @@ exports.responses = function(req, res){
 res.render('responses', { title: 'Reader Responses'});
 };
 
+exports.writing = function(req, res){
+res.render('writing', { title: 'Writing'});
+};
+
 exports.motherlessdaughters = function(req, res){
 res.render('motherlessdaughters', { title: 'Motherless Daughters'});
 };
